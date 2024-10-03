9a966660939b0849e3cfae3c67b8dfccb04a1854
Azer Ben Azzouz <benazzouzazer@gmail.com>
2024-10-03 15:49:09 +0100

    gg

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,7 +6,6 @@ import javax.xml.ws.Endpoint;
 public class Application {
     public static void main(String[] args) {
         compteRepository cr = new compteRepository();
-
         String url="http://localhost:8044/";
         Endpoint.publish(url, new bankService(cr));
         System.out.println(url);
